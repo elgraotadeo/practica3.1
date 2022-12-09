5a70cde374b503e870407a9aa1e23ac135d791c4
elgraotadeo <elgraotadeo.11608096@ieselgrao.org>
2022-12-09 09:34:45 +0100

    Version 1.0

---
+++ config.js
@@ -0,0 +1,5 @@
+function welcome(){
+    var name = document.getElementById('name').value;
+    var surname = document.getElementById('surname').value;
+    alert("Bienvenido " + name + " " + surname);
+}
